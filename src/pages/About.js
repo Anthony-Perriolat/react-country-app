e46caf94901a61029e129eaba9a6bb763d1b7d98
anthonyperriolat <pro.aperriolat@gmail.com>
2022-10-17 14:43:10 +0200

    first tuto react

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation />
+            <h1>à propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum impedit fugit, itaque architecto cupiditate suscipit quisquam explicabo quo dolore, repellendus voluptatum ut. Eum, quae ex alias adipisci mollitia sapiente numquam nobis dolorum quis asperiores libero laudantium totam! Aut a voluptatum, illo delectus sed eos id fuga quia praesentium illum distinctio placeat. Quidem totam neque provident fuga animi excepturi quia numquam ab tempore recusandae, officiis harum, perspiciatis adipisci dolore, architecto illo dolorum fugiat nesciunt cum libero odio deleniti magni illum aliquid? Sint excepturi nemo dolorum commodi obcaecati laudantium in autem sed omnis natus a doloremque accusamus quidem repellat ducimus quam dignissimos nam cum quasi atque numquam dolor, quia dolores quis? Porro vitae similique tempore obcaecati magnam quaerat vero harum nesciunt, ipsam eum. Laudantium nulla qui incidunt. Quis tempora, vel fuga praesentium harum tempore, id laudantium debitis illum reprehenderit ullam amet error corporis eaque cum ex quibusdam impedit ducimus consequatur officiis facilis?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum impedit fugit, itaque architecto cupiditate suscipit quisquam explicabo quo dolore, repellendus voluptatum ut. Eum, quae ex alias adipisci mollitia sapiente numquam nobis dolorum quis asperiores libero laudantium totam! Aut a voluptatum, illo delectus sed eos id fuga quia praesentium illum distinctio placeat. Quidem totam neque provident fuga animi excepturi quia numquam ab tempore recusandae, officiis harum, perspiciatis adipisci dolore, architecto illo dolorum fugiat nesciunt cum libero odio deleniti magni illum aliquid? Sint excepturi nemo dolorum commodi obcaecati laudantium in autem sed omnis natus a doloremque accusamus quidem repellat ducimus quam dignissimos nam cum quasi atque numquam dolor, quia dolores quis? Porro vitae similique tempore obcaecati magnam quaerat vero harum nesciunt, ipsam eum. Laudantium nulla qui incidunt. Quis tempora, vel fuga praesentium harum tempore, id laudantium debitis illum reprehenderit ullam amet error corporis eaque cum ex quibusdam impedit ducimus consequatur officiis facilis?</p>
+        </div>
+    );
+};
+
+export default About;
